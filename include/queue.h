5e82e5c06c3554dcc017ab2c0ed85811212c4f82
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-03-25 20:28:53 -0400

    we did so great it was fun!

--- include/queue.h
+++ include/queue.h
@@ -6,41 +6,39 @@
 /*			2 for sleep list plus 2 per semaphore		*/
 
 #ifndef NQENT
-#define NQENT	(NPROC + 4 + NSEM + NSEM)
+#define NQENT (NPROC + 4 + NSEM + NSEM)
 #endif
 
-#define	EMPTY	(-1)		/* null value for qnext or qprev index	*/
+#define EMPTY (-1) /* null value for qnext or qprev index	*/
 
-
-//TODO - define queue's data members
+// TODO - define queue's data members
 struct queue
 {
 	struct qentry *head; // TODO - pointer to head qentry
 	struct qentry *tail; // TODO - pointer to tail qentry
-	int32	size;	// TODO - size of queue
+	int32 size;			 // TODO - size of queue
 };
 
-
-//TODO - define a queue entry's data members
+// TODO - define a queue entry's data members
 struct qentry
 {
-	pid32 pid;	// TODO - process ID
-	struct qentry *next;	// TODO - other members
+	pid32 pid;			 // TODO - process ID
+	struct qentry *next; // TODO - other members
 	struct qentry *prev;
+	int32 priority;
 };
 
-
 /* Queue function prototypes (don't touch!) */
-void    printqueue(struct queue *q);
+void printqueue(struct queue *q);
 
-bool8	isempty(struct queue *q);
-bool8	nonempty(struct queue *q);
-bool8	isfull(struct queue *q);
+bool8 isempty(struct queue *q);
+bool8 nonempty(struct queue *q);
+bool8 isfull(struct queue *q);
 
-pid32	getfirst(struct queue *q);
-pid32	getlast(struct queue *q);
-pid32	remove(pid32 pid, struct queue *q);
-pid32	dequeue(struct queue *q);
-pid32	enqueue(pid32 pid, struct queue *q);
-struct queue	*newqueue();
-struct qentry	*getbypid(pid32 pid, struct queue *q);
+pid32 getfirst(struct queue *q);
+pid32 getlast(struct queue *q);
+pid32 remove(pid32 pid, struct queue *q);
+pid32 dequeue(struct queue *q);
+pid32 enqueue(pid32 pid, struct queue *q, int32 key);
+struct queue *newqueue();
+struct qentry *getbypid(pid32 pid, struct queue *q);
--- system/resched.c
+++ system/resched.c
@@ -6,33 +6,37 @@
  * Reschedule processor to next ready process
  *
  */
-void	resched(void)		// assumes interrupts are disabled
+void resched(void) // assumes interrupts are disabled
 {
-	struct procent *ptold;	// ptr to table entry for old process
-	struct procent *ptnew;	// ptr to table entry for new process
+	struct procent *ptold; // ptr to table entry for old process
+	struct procent *ptnew; // ptr to table entry for new process
 
 	// If rescheduling is deferred, record attempt and return
-	if (Defer.ndefers > 0) {
+	if (Defer.ndefers > 0)
+	{
 		Defer.attempt = TRUE;
 		return;
 	}
 
 	// Point to process table entry for the current (old) process
-// DC REMOVE
+	// DC REMOVE
 
 	ptold = &proctab[currpid];
 
-        if (ptold->prstate == PR_CURR) {
-                // Old process got preempted; place back on ready queue
-                ptold->prstate = PR_READY;
-                enqueue(currpid, readyqueue);
-        }
+	if (ptold->prstate == PR_CURR)
+	{
+		// Old process got preempted; place back on ready queue
+		pri16 prior = ptold->prprio;
+		ptold->prstate = PR_READY;
+		enqueue(currpid, readyqueue, prior);
+		printqueue(readyqueue);
+	}
 
-        // Force context switch to next ready process
-        currpid = dequeue(readyqueue);
-        ptnew = &proctab[currpid];
-        ptnew->prstate = PR_CURR;
-// DC REMOVE END 
+	// Force context switch to next ready process
+	currpid = dequeue(readyqueue);
+	ptnew = &proctab[currpid];
+	ptnew->prstate = PR_CURR;
+	// DC REMOVE END
 	// TODO - check ptold's state. If it's running, put it on the ready queue and change state to ready
 
 	// TODO - dequeue next process off the ready queue and point ptnew to it
